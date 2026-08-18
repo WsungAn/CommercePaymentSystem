f50ad4c33e5deb614b5ed512204f3f540b2ebb42
wsungan <ws960221@gmail.com>
2026-08-18 23:50:30 +0900

    member entity 수정

--- src/main/java/com/example/commercepaymentsystem/member/entity/Member.java
+++ src/main/java/com/example/commercepaymentsystem/member/entity/Member.java
@@ -13,12 +13,13 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length = 50)
+    @Column(length = 50, nullable = false)
     private String name;
     @Column(length = 100, unique = true)
     private String email;
-    @Column(length = 20, unique = true)
+    @Column(length = 20, nullable = false, unique = true)
     private String phoneNumber;
+    @Column(nullable = false, length = 255)
     private String password;
 
     public Member(String name, String email, String phoneNumber, String password) {
