55aee0087c3aac9c3c165facb926349c13e0e137
wonyeolShin <lwy171400@gmail.com>
2026-08-19 17:39:31 +0900

    [Cart] cart CRUD 구현 (3-layer)

--- src/main/java/com/example/commercepaymentsystem/domain/cart/facade/CartFacade.java
+++ src/main/java/com/example/commercepaymentsystem/domain/cart/facade/CartFacade.java
@@ -1,7 +1,10 @@
 package com.example.commercepaymentsystem.domain.cart.facade;
 
+import com.example.commercepaymentsystem.common.exception.BusinessException;
+import com.example.commercepaymentsystem.common.exception.ErrorCode;
 import com.example.commercepaymentsystem.domain.cart.dto.CartResponse;
 import com.example.commercepaymentsystem.domain.cart.entity.Cart;
+import com.example.commercepaymentsystem.domain.cart.entity.CartItem;
 import com.example.commercepaymentsystem.domain.cart.service.CartItemService;
 import com.example.commercepaymentsystem.domain.cart.service.CartService;
 import com.example.commercepaymentsystem.domain.member.entity.Member;
@@ -10,12 +13,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
-@Transactional
 public class CartFacade {
 
     // 상품 담기를 시도한 사용자가 유효한지, 장바구니를 가지고 있는지  등 검증을 위해 사용
@@ -27,7 +29,7 @@ public class CartFacade {
     // 사용자가 담은 상품과 수량을 확인하기 위해 사용
     private final CartItemService cartItemService;
 
-    // 상품 담기 요청
+    // 상품 담기 요청, 요청한 수량만큼 상품을 담는다
     public void addProductToCart(
             Long memberId, Long productId, int quantity)
     {
@@ -49,25 +51,86 @@ public class CartFacade {
         // 장바구니에 담기 상품의 재고보다 장바구니에 담은 수량이 더 많은지 검증하는 로직
         int currentCartItemQuantity = cartItemService.getExistingQuantity(cart, product);
         int totalQuantity = currentCartItemQuantity + quantity;
-        // 상품 구현이 완료되면 재고 검증을 하는 로직 사용
-        productService.validateStock(totalQuantity);
+
+        // 장바구니에 넣은 수량이 현재 상품의 재고보다 많음
+        if (productService.validateStock(totalQuantity, product )){
+            throw new BusinessException(ErrorCode.INSUFFICIENT_STOCK);
+        }
 
         // cart_id + product_id 조합으로 장바구니 수량 증감
         cartItemService.addCartItem(cart, product, quantity);
 
     }
 
+    // 장바구니 조회 요청 상품정보(id,name,price)와 수량, 총 가격을 전달한다.
     public CartResponse getCart(Long memberId) {
 
-        // 요청한 멤버가 있는지 확인
-        memberService.findMember(memberId);
+        // 유저의 장바구니를 꺼내옴
+        Optional<Cart> cart = cartService.getCart(memberId);
 
-        // 유저의 장바구니를 꺼내옴 없으면 장바구니 생성
+        // 장바구니가 없는 사용자라면 그냥 빈 상품과 총금액 0원으로 전달한다.
+        if (cart.isEmpty()) {
+            // 빈 List가 아닌 null로 전달하면 프론트에서 한번 더 로직을 타야되서 힘듦
+            return new CartResponse(List.of(),0);
+        }
 
+        // 사용자의 장바구니에 있는 상품 정보를 dto로 변환
+        return cartItemService.getCartItems(cart.get()) ;
+    }
 
-        // 장바구니에 있는 상품 정보들을 가져온현
-        List<Long> productIds = new ArrayList<>();
+    // 장바구니에 들어있는 상품의 수령을 변경한다.
+    public void changeProductToCart(Long memberId,  Long cartItemId, int changeQuantity )
+    {
 
-        return null;
+        // 요청한 사용자가 소지하고 있는 장바구니를 꺼내옴
+        Optional<Cart> cart = cartService.getCart(memberId);
+
+        // 해당 사용자의 장바구니가 존재하지 않는다면 예외
+        if (cart.isEmpty()) {
+            throw new BusinessException(ErrorCode.CART_EMPTY);
+        }
+
+        // 변경 요청한 cartItemId이 요청한 사용자 소유인지 확인 후 가져다줌
+        CartItem cartItem = cartItemService.checkCartItemAuthor(cartItemId, cart.get());
+
+        // 변경하려는 수량이 현재 상품의 재고보다 많음
+        if (productService.validateStock(changeQuantity, cartItem.getProduct() )){
+            throw new BusinessException(ErrorCode.INSUFFICIENT_STOCK);
+        }
+
+        // 본인 소유의 cartItem이 맞고 수량도 일치하다면 변경 진행
+        cartItemService.changeCartItem(cartItem, changeQuantity);
+
+    }
+
+    // 장바구니에 들어있는 상품을 삭제한다.
+    public void deleteProductToCart(Long memberId, Long cartItemId) {
+
+        // 요청한 사용자가 소지하고 있는 장바구니를 꺼내옴
+        Optional<Cart> cart = cartService.getCart(memberId);
+
+        // 해당 사용자의 장바구니가 존재하지 않는다면 예외
+        if (cart.isEmpty()) {
+            throw new BusinessException(ErrorCode.CART_EMPTY);
+        }
+
+        // 변경 요청한 cartItemId이 요청한 사용자 소유인지 확인 후 가져다줌
+        CartItem cartItem = cartItemService.checkCartItemAuthor(cartItemId, cart.get());
+
+        cartItemService.deleteCartItem(cartItem);
+    }
+
+    // 장바구니에 들어있는 "모든" 상품을 삭제한다.
+    public void deleteAllProductToCart(Long memberId) {
+
+        // 요청한 사용자가 소지하고 있는 장바구니를 꺼내옴
+        Optional<Cart> cart = cartService.getCart(memberId);
+
+        // 해당 사용자의 장바구니가 존재하지 않는다면 예외
+        if (cart.isEmpty()) {
+            throw new BusinessException(ErrorCode.CART_EMPTY);
+        }
+
+        cartItemService.deleteCartItems(cart.get());
     }
 }
--- src/main/java/com/example/commercepaymentsystem/domain/cart/service/CartService.java
+++ src/main/java/com/example/commercepaymentsystem/domain/cart/service/CartService.java
@@ -1,14 +1,39 @@
 package com.example.commercepaymentsystem.domain.cart.service;
 
+import com.example.commercepaymentsystem.domain.cart.entity.Cart;
 import com.example.commercepaymentsystem.domain.cart.repository.CartRepository;
+import com.example.commercepaymentsystem.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CartService {
 
     private final CartRepository cartRepository;
+
+    // 장바구니에 담을 때 유저의 장바구니를 꺼내옴 없으면 장바구니 생성
+    public Cart getOrCreateCart(Member member) {
+
+        Optional<Cart> cart = cartRepository.findById(member.getId());
+
+        if (cart.isEmpty()) {
+            Cart newCart = new Cart(member);
+            return cartRepository.save(newCart);
+        }
+
+        return cart.get();
+    }
+
+    // 유저가 가지고 있는 장바구니를 꺼내온다.
+    @Transactional(readOnly = true)
+    public Optional<Cart> getCart(Long memberId) {
+        return cartRepository.findById(memberId);
+    }
 
 
     // 현재
