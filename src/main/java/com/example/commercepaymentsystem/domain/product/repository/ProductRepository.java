db54435e0e7cdff79b17375813196f085c9cda40
junyho <inop159@naver.com>
2026-08-19 11:04:46 +0900

    refactor: package 이동

---
+++ src/main/java/com/example/commercepaymentsystem/domain/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.commercepaymentsystem.domain.product.repository;
+
+import com.example.commercepaymentsystem.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
