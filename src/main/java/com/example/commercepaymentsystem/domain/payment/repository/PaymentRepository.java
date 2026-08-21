9da05077e0ab5e9797af2bc012716958ff21c153
na00111 <angela715@naver.com>
2026-08-21 20:58:55 +0900

    feat(payment): 리포지토리 구현

--- src/main/java/com/example/commercepaymentsystem/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/commercepaymentsystem/domain/payment/repository/PaymentRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Optional<Payment> findByOrderIdAndMemberId(Long orderId, Long memberId);
+    Optional<Payment> findByOrder_IdAndMember_Id(Long orderId, Long memberId);
 
     //주문 단건 조회 화면 -> 결제 ID 한 건만 필요하므로 payment 엔티티 전체를 로딩하지 않고 ID만 프로젝션
     @Query("SELECT p.id FROM Payment p WHERE p.order.id =:orderId")
